eca1659cc8d19e86062e115820ad086530951f9a
mohammad reza jelveh <mrj.eleven@gmail.com>
2021-07-10 01:09:05 +0430

    adding containers and component to test

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            It's header
+        </div>
+    )
+}
+
+export default Header
---
+++ src/containers/dashboard/dashboard.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../components/header/header.component";
+
+const Dashboard = () => {
+    return(
+        <div>
+            <Header />
+            I'm Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
