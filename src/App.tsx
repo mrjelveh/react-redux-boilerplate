eeab0610ba256d23938dbdba963b7c8b80c16953
mohammad reza jelveh <mrj.eleven@gmail.com>
2021-12-22 21:28:29 +0330

    config test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-
 import React from "react";
-import {withRouter, BrowserRouter as Router, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
 import Dashboard from "./containers/dashboard/dashboard.page";
 import Login from "./containers/login/login.page";
 import PrivateRoute from "./routes/private-route";
 import PublicRoute from "./routes/public-route";
 
-
 function App() {
   return (
     <Router>
-    <Header />
-    <Switch>
-        <PublicRoute path='/login' component={Login} />
-        <PrivateRoute path='/' roles={'all'} component={Dashboard} />
-    </Switch>
-</Router>
-  )
+      <Header />
+      <Switch>
+        <PublicRoute path="/login" component={Login} />
+        <PrivateRoute path="/" roles={"all"} component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
