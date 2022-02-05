94469ed2bc9e982d9e62c6e8e1c6d51ed21d7d38
mohammad reza jelveh <mrj.eleven@gmail.com>
2022-02-05 17:00:18 +0330

    Add some dummy test for showing structure of unit test

--- src/components/header/__test__/header.test.tsx
+++ src/components/header/__test__/header.test.tsx
@@ -1,10 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../header.component";
+
 // NOTE: it is a dummy test file just for showing structure of test file,
 //       so it is not a real test file.
 // If you want know more about testing, please visit:
 // https://testing-library.com/docs/react-testing-library/intro
-
-test("renders app project", () => {
-  expect(true).toBeTruthy();
+test("should render header component", () => {
+  render(<Header />);
+  screen.debug();
 });
-
-export {};
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {ReactComponent as Logo} from "../../assets/img/logo.svg";
+import { ReactComponent as Logo } from "../../assets/img/logo.svg";
 
 function Header() {
-    return (
-        <div className="app">
-            <Logo className="h-64" />
-        </div>
-    )
+  return (
+    <div className="app">
+      <Logo className="h-64" />
+    </div>
+  );
 }
 
-export default Header
+export default Header;
