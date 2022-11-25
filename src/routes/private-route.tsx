e377f1cf44eec53bd050be06562dbb3f706c26d7
mohammad reza jelveh <mrj.eleven@gmail.com>
2022-11-25 23:54:02 +0330

    Update components and pages

--- src/routes/private-route.tsx
+++ src/routes/private-route.tsx
@@ -1,35 +1,17 @@
-import React from 'react';
-import {Route, Redirect, RouteComponentProps} from 'react-router-dom';
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 import roleRequest from "./role-request";
-import {useAppSelector} from "../redux/store";
-import {selectLoginState} from "../redux/authentication/authentication.slice";
+import { useAppSelector } from "../redux/store";
+import { selectLoginState } from "../redux/authentication/authentication.slice";
 
 type PRIVATEROUTETYPE = {
-    roles: string,
-    exact?: boolean,
-    path?: string,
-    component: React.FC<RouteComponentProps>,
-}
-const PrivateRoute: React.FC<PRIVATEROUTETYPE> = ({
-      roles,
-      component: Component,
-      path,
-      exact = false,
-      ...otherProps}) => {
-    const { isAuth } = useAppSelector(selectLoginState)
-    return (
-        <Route path={path} exact={exact} {...otherProps} component={
-            (props: RouteComponentProps) => (
-                roleRequest(roles) && isAuth ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to="/login" />
-                )
-            )
-        }>
-            
-        </Route>
-    )
-}
+  roles: string[];
+};
 
-export default PrivateRoute
+const PrivateRoutes: React.FC<PRIVATEROUTETYPE> = ({ roles }) => {
+  const { isAuth, permittedRole } = useAppSelector(selectLoginState);
+  const isTrustedRole = roleRequest(roles, permittedRole);
+  return isTrustedRole && isAuth ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoutes;
